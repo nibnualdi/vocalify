a40ccc5bf93de24c707623260098fb7b84bfb0a9
nibnualdi <nibnualdi@gmail.com>
2023-11-17 09:23:28 +0700

    ref: remove the useless code

--- app/_layout.js
+++ app/_layout.js
@@ -1,20 +1,13 @@
 import { useFonts } from "expo-font";
 import { Slot } from "expo-router";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
-import { Audio } from "expo-av";
 
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  setAudioTitleAndArtist,
-  setIsPlaying,
-  setIsloading,
-} from "../redux/features/audioPlayerSlice";
+import { useSelector } from "react-redux";
 
 import { Header, Navbar, SlidingUp } from "../components";
-import { useGetAllSongsQuery } from "../redux/services/song";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -32,67 +25,8 @@ const _layout = () => {
     "Inter-Regular": require("../assets/fonts/Inter-Regular.otf"),
     "Inter-Bold": require("../assets/fonts/Inter-Bold.otf"),
   });
-  // const [sound, setSound] = useState();
-  // const dispatch = useDispatch();
 
-  // const playNewSound = async () => {
-  //   console.log("Loading Sound");
-  //   dispatch(setAudioTitleAndArtist({ title: "coba dulu aja", artistName: "ehem" }));
-  //   dispatch(setIsloading(true));
-  //   dispatch(setIsPlaying(true));
-  //   const { sound } = await Audio.Sound.createAsync({
-  //     uri: "https://firebasestorage.googleapis.com/v0/b/vocalify-d0c53.appspot.com/o/x2mate.com%20-%20%F0%9F%8D%80%20Chill%20Instrumental%20%5BNon%20Copyrighted%20Music%5D%20_Embrace_%20by%20%40Sappheiros%20%20%F0%9F%87%BA%F0%9F%87%B8%20(128%20kbps).mp3?alt=media&token=858b0998-7189-480c-ab9a-14278f16d414",
-  //   });
-  //   setSound(sound);
-
-  //   console.log("Playing Sound");
-  //   await sound.playAsync();
-  //   dispatch(setIsloading(false));
-  // };
-
-  // const playSound = async () => {
-  //   console.log("play Sound");
-  //   await sound.playAsync();
-  //   dispatch(setIsPlaying(true));
-  // };
-
-  // const stopSound = async () => {
-  //   console.log("Pause Sound");
-  //   await sound.pauseAsync();
-  //   dispatch(setIsPlaying(false));
-  // };
-
-
-  // here
-  const dispatch = useDispatch();
   const song = useSelector((state)=>state.audioPlayer.song);
-
-  const playNewSound = async ({ title, artistName, songUrl, song }) => {
-    console.log("Loading Sound");
-    dispatch(setIsloading(true));
-    dispatch(setIsPlaying(true));
-    const { sound } = await Audio.Sound.createAsync({
-      uri: songUrl,
-    });
-    dispatch(setAudioTitleAndArtist({ title, artistName, song }));
-
-    console.log("Playing Sound");
-    await sound.playAsync();
-    dispatch(setIsloading(false));
-  };
-
-  const playSound = async () => {
-    console.log("play Sound");
-    await song.playAsync();
-    dispatch(setIsPlaying(true));
-  };
-
-  const stopSound = async () => {
-    console.log("Pause Sound");
-    await song.pauseAsync();
-    dispatch(setIsPlaying(false));
-  };
-  // here
 
   useEffect(() => {
     return song
@@ -102,15 +36,6 @@ const _layout = () => {
         }
       : undefined;
   }, [song]);
-
-  // useEffect(() => {
-  //   return sound
-  //     ? () => {
-  //         console.log("Unloading Sound");
-  //         sound.unloadAsync();
-  //       }
-  //     : undefined;
-  // }, [sound]);
 
   // load fonts
   useEffect(() => {
@@ -130,7 +55,6 @@ const _layout = () => {
     <>
       <Header />
       <Slot />
-      {/* <SlidingUp playNewSound={playNewSound} playSound={playSound} stopSound={stopSound} /> */}
       <SlidingUp />
       <Navbar />
     </>
