ecb0bad54195a6768c932793cc18d31db6958870
nibnualdi <nibnualdi@gmail.com>
2023-11-17 20:06:07 +0700

    feat: add a solution for sliding up behavior (still not perfect, that's why it's commented)

--- components/SlidingUp.js
+++ components/SlidingUp.js
@@ -7,6 +7,7 @@ import NextButton from "./AudioPlayer/NextButton";
 
 const SlidingUp = () => {
   let ref = useRef();
+  let ref2 = useRef();
   const animatedValue = new Animated.Value(0);
   const { height } = Dimensions.get("window");
   const imageUrl = useSelector((state) => state.audioPlayer.imageUrl);
@@ -25,8 +26,17 @@ const SlidingUp = () => {
   //   }
   //   console.log("title: ", title)
   // }, [title])
+  
+  // solution 2 but the behavior of the velocity is strange
+  // and the user still can drop all the way down the sliding
+  // useEffect(()=>{
+  //   if(title) {
+  //     ref.show(140, 1000)
+  //   }
+  //   console.log("title: ", title)
+  // }, [title])
 
-  if(!imageUrl) return
+  if (!imageUrl) return;
 
   return (
     <SlidingUpPanel
@@ -34,24 +44,47 @@ const SlidingUp = () => {
       snappingPoints={[140]}
       animatedValue={animatedValue}
       draggableRange={{ top: height, bottom: 140 }}
-      friction={0.8}
+      friction={1}
+      backdropOpacity={0}
     >
-      <View style={styles.container}>
-        <Image source={{
-          uri: imageUrl
-        }} style={styles.image} width={50} height={50} />
+      <>
+        <View style={styles.container}>
+          <Image
+            source={{
+              uri: imageUrl ? imageUrl : "https://dpcpa.com/app/uploads/2015/01/thumbnail-default.jpg",
+            }}
+            style={styles.image}
+            width={50}
+            height={50}
+          />
 
-        <View style={styles.textContainer}>
-          <Text style={styles.title} numberOfLines={1}>{title}</Text>
-          <Text style={styles.subTitle} numberOfLines={1}>{artistName}</Text>
-        </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.title} numberOfLines={1}>
+              {title}
+            </Text>
+            <Text style={styles.subTitle} numberOfLines={1}>
+              {artistName}
+            </Text>
+          </View>
 
-        <View style={styles.containerPlayPauseButton}>
-          <PlayPauseButton />
-        </View>
-        {/* <NextButton />
+          <View style={styles.containerPlayPauseButton}>
+            <PlayPauseButton />
+          </View>
+          {/* <NextButton />
         <Button title="Hide" onPress={() => ref.hide()} /> */}
-      </View>
+        </View>
+
+        <SlidingUpPanel
+          ref={(c) => (ref2 = c)}
+          snappingPoints={[140]}
+          animatedValue={animatedValue}
+          draggableRange={{ top: height - 95, bottom: 140 }}
+          friction={0.8}
+          backdropOpacity={0}
+        >
+          <Text style={{width: "100%", height: "100%", backgroundColor: "green"}}>Here is the lyric and next songs tabs</Text>
+        </SlidingUpPanel>
+      </>
     </SlidingUpPanel>
   );
 };
@@ -64,7 +97,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     backgroundColor: "white",
-    padding: 10
+    padding: 10,
   },
   image: {
     flex: 1,
@@ -75,10 +108,10 @@ const styles = StyleSheet.create({
     height: 50,
   },
   title: {
-    width: 100
+    width: 100,
   },
   subTitle: {
-    width: 100
+    width: 100,
   },
   containerPlayPauseButton: {
     flex: 1,
@@ -86,4 +119,4 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-})
+});
